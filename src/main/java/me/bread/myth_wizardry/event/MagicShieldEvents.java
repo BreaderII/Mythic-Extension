469e798f0bf4e0946ee3a55336b71bf186d57d29
BreaderII <blackbreaderii@mail.ru>
2026-06-04 14:08:41 +0300

    adding MagicShield spell

--- src/main/java/me/bread/myth_wizardry/event/MagicShieldEvents.java
+++ src/main/java/me/bread/myth_wizardry/event/MagicShieldEvents.java
@@ -5,22 +5,16 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import me.bread.myth_wizardry.MythWizardry;
 import me.bread.myth_wizardry.models.MagicShieldModel;
-import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.joml.Matrix4f;
-import org.joml.Vector3fc;
 
 @Mod.EventBusSubscriber(modid = MythWizardry.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class MagicShieldEvents {
