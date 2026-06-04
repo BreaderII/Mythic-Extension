1c8671c3f8b4d1dd47386d69356e98d64ae8c635
BreaderII <blackbreaderii@mail.ru>
2026-06-04 14:03:36 +0300

    adding MagicShield spell

---
+++ src/main/java/me/bread/myth_wizardry/effects/MagicShield.java
@@ -0,0 +1,10 @@
+package me.bread.myth_wizardry.effects;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class MagicShield extends MobEffect {
+    public MagicShield() {
+        super(MobEffectCategory.BENEFICIAL, 0x00_00_00_00);
+    }
+}
